2b618c7856b8355813d625fba19ca37fc825af0b
Immanuelprasad <82777514+Immanuelprasad@users.noreply.github.com>
2021-04-19 02:14:44 +0530

    .

---
+++ ComparisonOperators/ComparisonOperatorsDemo2.py
@@ -0,0 +1,8 @@
+name = "Park Chaeyoung"
+
+if len(name) < 5:
+    print("Name must be at least 5 characters")
+elif len(name) > 50:
+    print("Name can be maximum of 50 characters")
+else:
+    print("Name looks good!")
